f18ee63754c0bcbad8b5ad4311146d42e5fd3fcd
GMA950 <francobocca.97@gmail.com>
2021-11-01 19:37:18 -0300

    avance de sprint

---
+++ inventario-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '97cf6e9a-75ef-4b5b-a790-b0adc5279141'
+};
--- inventario-v2/src/components/InvSection.jsx
+++ inventario-v2/src/components/InvSection.jsx
@@ -1,16 +1,91 @@
 import React from 'react';
+import axios from 'axios';
 import '../App.css';
 import { Button } from './Button';
 import './InvSection.css';
+import StockEventsTable from './StockEventsTable';
+
+// 2 Data Types
+
+// Products
+
+const fetchedProducts = [
+    {id: 1, name: "Motor"},
+    {id: 2, name: "Rueda"},
+    {id: 3, name: "Aromatizante"}
+]
+
+// stock events
+
+const fetchedStockEvents = [
+    {id: 1, type: 'add', qty: 100, product: fetchedProducts[0]},
+    {id: 2, type: 'remove', qty: -20, product: fetchedProducts[0]},
+    {id: 3, type: 'remove', qty: -10, product: fetchedProducts[0]},
+
+    {id: 4, type: 'add', qty: 120, product: fetchedProducts[1]},
+    {id: 5, type: 'remove', qty: -45, product: fetchedProducts[1]},
+]
+
+//fetch stock events
 
 
-function HeroSection() {
+// separate by product
+//display
+
+class InvSection extends React.Component{
+    state = {
+        fetchedProducts,
+        fetchedStockEvents
+    }
+
+    async componentDidMount(){
+
+        const productsRes = await axios({
+            method: 'GET', 
+            url: 'http://localhost:1337/products'
+        })
+
+        const stockEventsRes = await axios({
+            method: 'GET', 
+            url: 'http://localhost:1337/stockevents'
+        })
+        
+        //console.log("App.componentDidMount stockEventsRes", stockEventsRes)
+
+        const fetchedProducts = productsRes.data
+        const fetchedStockEvents = stockEventsRes.data
+
+        this.setState({fetchedProducts, fetchedStockEvents})
+    }
+
+    render(){
+        console.log("App.render")
+        const {fetchedProducts, fetchedStockEvents} = this.state
+        return (
+            <div className ='inv-container'>
+                <h1>Inventario</h1>
+                <StockEventsTable
+                    products={fetchedProducts} 
+                    stockEvents = {fetchedStockEvents}
+                />
+            </div>
+        );
+    }
+}
+
+/*
+function InvSection() {
     return (
         <div className ='inv-container'>
-            {/*<video src="/videos/video-2.mp4" autoPlay loop muted/>*/}
+            <video src="/videos/video-2.mp4" autoPlay loop muted/>
             <h1>Inventario</h1>
-            {/*<p>Inicio de Sesión</p>*/}
-            {/*<div className="hero-btns">
+            <StockEventsTable
+                products={fetchedProducts} 
+                stockEvents = {fetchedStockEvents}
+            />
+            
+            {/*<p>Inicio de Sesión</p>*/
+            /*<div className="hero-btns">
                 <Button className='btns' 
                 buttonStyle='btn--outline'
                 buttonSyze='btn--large'
@@ -23,8 +98,8 @@ function HeroSection() {
                 >
                     WATCH TRAILER <i className = 'far fa-play-circle' />
                 </Button>
-            </div>*/}
-            {/*
+            </div>*/
+            /*
             <div className='input-areas'>
             <form>
                 <input
@@ -42,9 +117,10 @@ function HeroSection() {
                 <Button buttonStyle='hero-btns' buttonStyle='btn--primary'>Iniciar Sesión</Button>
             </form>
             </div>
-            */}
+            /
         </div>
-    )
+    );
 }
+*/
 
-export default HeroSection
+export default InvSection;
