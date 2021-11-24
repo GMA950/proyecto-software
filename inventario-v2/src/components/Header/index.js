47aa0cc869f55f0c3f19a5f61e527efdf4fd4454
enzomarin <enzomarin12@gmail.com>
2021-11-24 02:39:46 -0300

    font-end login+inventario v2.1

--- inventario-v2/src/components/Header/index.js
+++ inventario-v2/src/components/Header/index.js
@@ -1,21 +1,70 @@
 
-import React from "react";
-import './header.css'
-import Navbar from "components/NavbarLogged";
+import React, { useState } from "react";
 import useUser from "hooks/useUser";
-
+import { Link } from "react-router-dom";
+import './header.css'
+import { useHistory } from "react-router-dom"
 
 export default function Header(){
     //const isLogged = true
+    const [isActive, setActive] = useState(false)
+    const pushLocation= useHistory()
     const {isLogged,logout} = useUser()
+
+    const handleClick = e =>{
+        e.preventDefault()
+        pushLocation.push('/')
+        logout()
+    }
+    const toggleClassName = () =>{
+        
+        setActive(!isActive)
+        console.log(isActive)
+    }
+    
     return(
         <header className= 'app-header'>
             {
                 isLogged
                     // este seria el navBar una vez iniciada la sesion
-                    ? <div>
-                        <Navbar></Navbar>
-                        <button onClick={logout}></button>
+                    ? <div className= 'header-container'>
+                        <div className= 'navbar-container'>
+                            <div className='nav-logo' >
+                                Valrepuestos<i className="fab fa-typo3"/>
+                            </div>
+       
+                            <button className= 'nav-menu-icon' onClick = {toggleClassName}>
+                                <i className='fas fa-bars' />
+                            </button>
+
+                            <ul className={isActive? 'nav-menu nav-menu_active': 'nav-menu'}>
+                            
+                            
+                                <li className='nav-menu-item'>
+                                    <Link to='/inv' className='nav-links'>
+                                        INICIO
+                                    </Link>
+                                </li>
+                                <li className='nav-menu-item'>
+                                    <Link to='/inv' className='nav-links' >
+                                        INVENTARIO
+                                    </Link>
+                                </li>
+                                <li className='nav-menu-item'>
+                                    <Link to='/Products' className='nav-links' >
+                                        CONFIGURACIÓN
+                                    </Link>
+                                </li>
+                                
+                                <button className = 'nav-btn' onClick={handleClick}>CERRAR SESION</button>
+                                
+                            </ul>
+                            
+                            
+                        </div>
+
+
+                       
                     </div>
                         
 
